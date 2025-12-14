5ad3a356f9c1cb709fccaee4eef936f38cad4aee
Lofp34 <136515564+Lofp34@users.noreply.github.com>
2025-12-14 13:27:44 +0100

    Improve math input focus and keypad usability

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -3,12 +3,15 @@ import { cn } from '../../utils/cn';
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-export const Input: React.FC<InputProps> = ({ className, ...props }) => (
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, ...props }, ref) => (
   <input
+    ref={ref}
     className={cn(
       'w-full rounded-lg border border-slate-700 bg-slate-900 px-3 py-2 text-slate-100 focus:border-indigo-500 focus:outline-none',
       className,
     )}
     {...props}
   />
-);
+));
+
+Input.displayName = 'Input';
